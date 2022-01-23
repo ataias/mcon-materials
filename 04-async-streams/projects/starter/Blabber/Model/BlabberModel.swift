8707666f5462fd807ed6314a309414f24b49135e
Ataias Pereira Reis <ataiasreis@gmail.com>
2022-01-23 10:29:29 -0300

    Share location with async continuation

--- 04-async-streams/projects/starter/Blabber/Model/BlabberModel.swift
+++ 04-async-streams/projects/starter/Blabber/Model/BlabberModel.swift
@@ -39,6 +39,7 @@ import UIKit
 class BlabberModel: ObservableObject {
   var username = ""
   var urlSession = URLSession.shared
+  private var delegate: ChatLocationDelegate?
 
   init() {
   }
@@ -48,6 +49,10 @@ class BlabberModel: ObservableObject {
 
   /// Shares the current user's address in chat.
   func shareLocation() async throws {
+    let location: CLLocation = try await withCheckedThrowingContinuation({ [weak self] continuation in
+      self?.delegate = ChatLocationDelegate(continuation: continuation)
+    })
+    print(location.description)
   }
 
   /// Does a countdown and sends the message.
