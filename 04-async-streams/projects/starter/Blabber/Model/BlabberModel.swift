dbbc2f1675593e473c8963563ee362d7b9b7134b
Ataias Pereira Reis <ataiasreis@gmail.com>
2022-01-23 10:36:59 -0300

    Use continuation to get human readable location

--- 04-async-streams/projects/starter/Blabber/Model/BlabberModel.swift
+++ 04-async-streams/projects/starter/Blabber/Model/BlabberModel.swift
@@ -52,7 +52,22 @@ class BlabberModel: ObservableObject {
     let location: CLLocation = try await withCheckedThrowingContinuation({ [weak self] continuation in
       self?.delegate = ChatLocationDelegate(continuation: continuation)
     })
-    print(location.description)
+    let address: String = try await withCheckedThrowingContinuation({ continuation in
+      AddressEncoder.addressFor(location: location) { address, error in
+        switch (address, error) {
+        case (nil, let error?):
+          continuation.resume(throwing: error)
+        case (let address?, nil):
+          continuation.resume(returning: address)
+        case (nil, nil):
+          continuation.resume(throwing: "Address encoding failed")
+        case let (address?, error?):
+          continuation.resume(returning: address)
+          print(error)
+        }
+      }
+    })
+    try await say("📍 \(address)")
   }
 
   /// Does a countdown and sends the message.
